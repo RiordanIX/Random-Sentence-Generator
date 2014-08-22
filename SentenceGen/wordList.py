2acae8095293b161e0a0f531449f333cc5f871f5
Major Taylor <majorjctaylor@gmail.com>
2014-08-22 08:45:11 -0400

    Created wordList
    Can add more Nouns and regular verbs as desired.

---
+++ SentenceGen/wordList.py
@@ -0,0 +1,25 @@
+nouns = ['aardvark', 'animal', 'alimony', 'avatar', 'blip', 'bib', 'bucket',
+        'cat', 'cadaver', 'camp', 'crap', 'crocodile', 'cucaburro', 'donkey',
+        'elephant', 'fox', 'glove', 'hand', 'index', 'justice', 'wipe'
+        ]
+reg_verbs = ['attack', 'boot', 'crap', 'ditch', 'etch', 'fix', 'gripe', 'hex',
+            'invest', 'justify', 'kick', 'limp', 'mop', 'nod', 'open', 'poke',
+            'rage', 'stare', 'tear', 'underestimate', 'venerate', 'whisk',
+            'yell', 'zap'
+            ]
+            
+articles = ['a', 'the']
+
+conjunctions = ['and', 'but', 'or', 'with', 'while']
+
+obj_pronouns = ['I', 'she', 'he', 'it', 'you']
+
+ind_obj_pronouns = ['me', 'her', 'him', 'it', 'you']
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+###### Global variables ######
+
+numNouns = len(nouns) - 1
+
+numVerbs = len(reg_verbs) - 1
